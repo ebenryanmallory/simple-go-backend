f69e3afb374a1aab5a315fbce0875dd95db2ef35
Eben Mallory <ebenryanmallory@gmail.com>
2019-11-06 10:43:17 -0600

    simple template setup

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type Hello struct {
+    Greeting string
+}
+
+//Go application entrypoint
+func main() {
+
+	//We tell Go exactly where we can find our html file. We ask Go to parse the html file (Notice
+	// the relative path). We wrap it in a call to template.Must() which handles any errors and halts if there are fatal errors
+
+	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
+
+	//Our HTML comes with CSS that go needs to provide when we run the app. Here we tell go to create
+	// a handle that looks in the static directory, go then uses the "/static/" as a url that our
+	//html can refer to when looking for our css and other files.
+
+	http.Handle("/static/", //final url can be anything
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static")))) //Go looks in the relative "static" directory first using http.FileServer(), then matches it to a
+	//url of our choice as shown in http.Handle("/static/"). This url is what we need when referencing our css files
+	//once the server begins. Our html code would therefore be <link rel="stylesheet"  href="/static/stylesheet/...">
+	//It is important to note the url in http.Handle can be whatever we like, so long as we are consistent.
+
+	//This method takes in the URL path "/" and a function that takes in a response writer, and a http request.
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        data := Hello{
+            Greeting: "Stranger",
+        }
+		//If errors show an internal server error message
+		//I also pass the welcome struct to the welcome-template.html file.
+		if err := templates.ExecuteTemplate(w, "welcome-template.html", data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
+	//Print any errors from starting the webserver using fmt
+	fmt.Println("Listening on :8080")
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
